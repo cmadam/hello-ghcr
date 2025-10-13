70c3ccbfa8b5f2a2f091dd0d7c7ab4c59768f512
Constantin Adam <cmadam@us.ibm.com>
2025-10-13 10:10:19 -0400

    Change log message
    Signed-off-by: Constantin Adam <cmadam@us.ibm.com>

--- app.py
+++ app.py
@@ -1,11 +1,11 @@
 import time
 import logging
 
-"""A simple logger that prints 10 times “Hello World” every 5 seconds."""
+"""A simple logger that prints 10 times a message every 5 seconds."""
 
 logging.basicConfig(level=logging.INFO, format="%(asctime)s - %(message)s")
 
 if __name__ == "__main__":
     for i in range(10):
-        logging.info("Hello World")
+        logging.info("Example demonstrating how to bring your own image in a workflow.")
         time.sleep(5)
