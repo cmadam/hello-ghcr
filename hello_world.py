7d5793ff0bdc8c7dea86421ad7c751bc8118d472
Constantin Adam <cmadam@us.ibm.com>
2025-10-16 13:56:37 -0400

    Reduce number of iterations from 1000 to 120
    Signed-off-by: Constantin Adam <cmadam@us.ibm.com>

--- hello_world.py
+++ hello_world.py
@@ -2,12 +2,12 @@ import json
 import time
 import logging
 
-"""A simple logger that prints 1000 times a message every 5 seconds."""
+"""A simple logger that prints 120 times a message every 5 seconds."""
 
 logging.basicConfig(level=logging.INFO, format="%(asctime)s - %(message)s")
 
 if __name__ == "__main__":
-    for i in range(1000):
+    for i in range(120):
         logging.info("Example demonstrating how to bring your own image in a workflow.")
         time.sleep(5)
         if i % 10 == 0:
@@ -23,5 +23,4 @@ if __name__ == "__main__":
                 "gb_data_ready": {filename: "path"},
                 "gb_new_artifact": {"path": filename}
             }
-
             logging.info(json.dumps(dct))
