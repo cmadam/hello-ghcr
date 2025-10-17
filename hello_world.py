029bd06d474e520e37029d93b0a928c4601ffa5f
Constantin Adam <cmadam@us.ibm.com>
2025-10-17 10:23:19 -0400

    Add sample fileset/model location and output folder parameters
    Signed-off-by: Constantin Adam <cmadam@us.ibm.com>

--- hello_world.py
+++ hello_world.py
@@ -1,17 +1,54 @@
+import argparse
 import json
-import time
 import logging
+import os
+import sys
+import time
 
 """A simple logger that prints 120 times a message every 5 seconds."""
+
+
+# start_command:
+#   "/app/bootstrap_sdk.sh
+#      --fileset-location {{ bindings.input_artifact_path.binding.path }}
+#      --model-location {{ bindings.model_to_use.binding.path }}
+#      --output-custom-path {{ bindings.custom.binding.path }}
+#      --blah blah
+
 
 logging.basicConfig(level=logging.INFO, format="%(asctime)s - %(message)s")
 
 if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="Sample app")
+    parser.add_argument(
+        "--fileset-location",
+        type=str,
+        required=True,
+        help="The name of the fileset used by this build",
+    )
+    parser.add_argument(
+        "--model-location",
+        type=str,
+        required=True,
+        help="The name of the model used by this build",
+    )
+    parser.add_argument(
+        "--output-custom-path",
+        type=str,
+        required=True,
+        help="The name of the output path used by this build",
+    )
+    args = parser.parse_args()
+
+    logging.info("The fileset used by this build is located at %s", args.fileset_location)
+    logging.info("The model used by this build is located at %s", args.model_location)
+    logging.info("The output folder of this build is at %s", args.output_custom_path)
+
     for i in range(120):
         logging.info("Example demonstrating how to bring your own image in a workflow.")
         time.sleep(5)
         if i % 10 == 0:
-            filename = f"artifact_{i}.txt"
+            filename = os.path.join(args.output_custom_path, f"artifact_{i}.txt")
             with open(filename, "w") as fp:
                 fp.write(f"This is artifact {i}\n")
             dct = {
